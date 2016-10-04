a8d087e11d63e9cd2d2067892c0bc748e2c92d9e
lava m <lavanymkanna@gmail.com>
2016-10-04 16:52:22 -0400

    new user list working

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ function mainController($scope){
             state:'KY',
             zip:'40508'
         }
-    }];
+    },
+    {
+        name: 'second name',
+        title: 'CTO',
+        email:'@gmail',
+        phone:'333',
+        address:{
+            street:'1000 E Main',
+            city:'Lexington',
+            state:'KY',
+            zip:'40507'
+        }
+    }
+    ];
     $scope.newUser={
         name: '',
         title:'',
@@ -26,10 +39,34 @@ function mainController($scope){
             zip:''
         }
     };
-    $scope.selectedUser= $scope.userList[0]
-    $scope.addUser = function(){
-        $scope.userList.push($scope.newUser)
+    $scope.selectedUser= $scope.userList[0];
+    $scope.setSelectedUser= function(user){
+        $scope.selectedUser = user;
+    }
+
+    $scope.editMode = false;
+    $scope.addMode =false;
+
+    $scope.doAction = function(action){
+        if(action === 0){
+            $scope.addMode =true;
+        }
+        else{
+            $scope.editMode =true;
+        }
+
+    }
+    
+    
+    $scope.addUser = function(a){
+        if(a===0){
+            $scope.userList.push($scope.newUser)
+        }
+        else{
+            $scop.userList = $scope.newUser
+        }
         $scope.newUser = {};
+        $scope.addMode=false;
     }
 
 
