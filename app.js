7df9b8c7d5d6a146c7b86382e5c84c8cd75c6f17
tmcothran <tmcothran@hotmail.com>
2016-10-05 08:53:36 -0400

    Can add and edit users.

--- app.js
+++ app.js
@@ -2,72 +2,53 @@ angular.module('app',[])
     .controller('MainCtrl', mainController);
 
 function mainController($scope){
-    $scope.userList=[{
-        name: 'first name',
-        title: 'CEO',
-        email:'www',
-        phone:'112',
-        address:{
-            street:'348 E Main',
-            city:'Lexington',
-            state:'KY',
-            zip:'40508'
+    $scope.userList=[
+        {
+            name: 'first name',
+            title: 'CEO',
+            email:'www',
+            phone:'112',
+            address:{
+                street:'348 E Main',
+                city:'Lexington',
+                state:'KY',
+                zip:'40508'
+            }
+        },
+        {
+            name: 'second name',
+            title: 'CTO',
+            email:'@gmail',
+            phone:'333',
+            address:{
+                street:'1000 E Main',
+                city:'Lexington',
+                state:'KY',
+                zip:'40507'
+            }
         }
-    },
-    {
-        name: 'second name',
-        title: 'CTO',
-        email:'@gmail',
-        phone:'333',
-        address:{
-            street:'1000 E Main',
-            city:'Lexington',
-            state:'KY',
-            zip:'40507'
-        }
-    }
-    ];
-    $scope.newUser={
-        name: '',
-        title:'',
-        email:'',
-        phone:'',
-        address:{
-            street:'',
-            city:'',
-            state:'',
-            zip:''
-        }
-    };
+        ];
+    $scope.newUser={};
     $scope.selectedUser= $scope.userList[0];
-    $scope.setSelectedUser= function(user){
-        $scope.selectedUser = user;
-    }
-
     $scope.editMode = false;
-    $scope.addMode =false;
-
-    $scope.doAction = function(action){
-        if(action === 0){
-            $scope.addMode =true;
-        }
-        else{
-            $scope.editMode =true;
-        }
-
-    }
-    
-    
-    $scope.addUser = function(a){
-        if(a===0){
-            $scope.userList.push($scope.newUser)
-        }
-        else{
-            $scop.userList = $scope.newUser
+    $scope.editUser = function(user) {
+        $scope.selectedUser = user;
+        $scope.editMode = true;
+    };
+    $scope.addUser = function(user) {
+        $scope.selectedUser = $scope.newUser;
+        $scope.editMode = true;
+    };
+    $scope.userForm = function(){
+        if ($scope.selectedUser != $scope.newUser) {
+            console.log('User already in user list');
+        } else {
+            $scope.userList.push($scope.selectedUser);
         }
         $scope.newUser = {};
-        $scope.addMode=false;
-    }
-
-
+        $scope.editMode = false;
+        };
 }
+
+
+
